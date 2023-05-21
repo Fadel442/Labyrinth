f46e57f7044e43af7bb8a87acb4a0d4a898cbd0d
BlueMoonZ <79092830+Fadel442@users.noreply.github.com>
2023-05-21 15:24:46 +0700

    timer
    Time.timeScale

--- Assets/SceneLoad.cs
+++ Assets/SceneLoad.cs
@@ -8,11 +8,13 @@ public class SceneLoad : MonoBehaviour
     public void SceneLoader(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
     }
 
     public void ReloadCurrScene()
     {
         var currScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currScene.name);
+        Time.timeScale = 1f;
     }
 }
